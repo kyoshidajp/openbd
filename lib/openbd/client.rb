937fe25abadf3ef13a397a6f63926f897a9bdd09
kyoshidajp <claddvd@gmail.com>
2017-02-12 10:18:43 +0900

    Add documentation comments

--- lib/openbd/client.rb
+++ lib/openbd/client.rb
@@ -3,12 +3,28 @@ require 'json'
 
 module Openbd
 
+  # Raised for request error.
   class RequestError < StandardError; end
 
   END_POINT = 'https://api.openbd.jp/v1'
 
+  # The Client class provides a wrapper to the openBD service
+  #
+  # @see https://openbd.jp/
+  # @example
+  #   require 'openbd'
+  #
+  #   client = Openbd::Client.new
+  #
+  #   # get
+  #   client.get('978-4-7808-0204-7')
   class Client
 
+    # call /get
+    #
+    # @param [String|Array] isbn ISBN
+    # @raise [RequestError] if the request is wrong
+    # @return [Hash] Biblio infomation
     def get(isbn)
       url = "#{END_POINT}/get"
       isbns = isbn_param(isbn)
@@ -18,12 +34,18 @@ module Openbd
       body(resp)
     end
 
+    # call /coverage
+    #
+    # @return [Array<String>] List of ISBN
     def coverage
       url = "#{END_POINT}/coverage"
       resp = httpclient.get(url)
       body(resp)
     end
 
+    # get HTTPClient
+    #
+    # @return [HTTPClient] HTTPClient
     def httpclient
       @httpclient ||= HTTPClient.new
     end
